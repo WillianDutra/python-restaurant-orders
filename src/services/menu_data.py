67d93b0a8701ed4c586b53418dfe056cd14c0b6f
Willian Dutra <contatowilliandutra@gmail.com>
2023-06-19 16:17:32 +0000

    fix: Arrumando erros de lint

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,14 +1,15 @@
 from models.ingredient import Ingredient  # noqa: F401, E261, E501
 from models.dish import Dish  # noqa: F401, E261, E501
 
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
         self.dishes = set()
 
-        with open(source_path, 'r') as file: 
+        with open(source_path, 'r') as file:
             data = file.readlines()
-            
+
             new_dish = None
             for line in range(1, len(data)):
                 line_data = data[line].split(',')
@@ -25,8 +26,3 @@ class MenuData:
 
                 if line == len(data) - 1:
                     self.dishes.add(new_dish)
-
-
-if __name__ == "__main__":
-    menu = MenuData("tests/mocks/menu_base_data.csv")
-    print(menu.dishes)
